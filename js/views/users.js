fc398d6d0892c6a4aa05f213e970386b3835cbd6
cuteProg <janeyjacktz@gmail.com>
2025-10-13 20:30:09 +0300

    usersloading..

--- js/views/users.js
+++ js/views/users.js
@@ -1,9 +1,9 @@
 export async function loadUsersView(){
     const usersUrl = new URL('../pages/users.html', import.meta.url).toString();
-// fetch('/users,html')
-//             .then(response => response.text())
-//             .then(data => {
-//                 document.getElementById('header').innerHTML = data;
-//             })
-//             .catch(error => console.error('Error loading header:', error));
+fetch('/users,html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('header').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading header:', error));
 }
